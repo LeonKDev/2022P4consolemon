39957e71406445319721eb70f1a6c6812d176c0d
leon kruijer <32770@ma-web.nl>
2022-05-09 15:11:15 +0200

    classes and functions

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class ConsoleMon
+    {
+      int Health = 5;
+      int Energy = 0;
+      string name = "licky";
+     
+
+        public void TakeDamage(int Damage)
+        {
+
+            Health =- Damage;
+        }
+
+        public void DepleteEnergy(int energy)
+        {
+            Energy =- energy;
+        }
+    }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class Skill
+    {
+        float damage = 0;
+        float energyCost = 0;
+        string name = "Lick";
+
+        public void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            caster.DepleteEnergy(5);
+            target.TakeDamage(5);
+
+        }
+    }
+
+
+}
