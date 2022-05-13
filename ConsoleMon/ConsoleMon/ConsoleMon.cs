a73c29db1dd46cae562ca1fa2082338e8fee45d5
leon kruijer <32770@ma-web.nl>
2022-05-13 12:20:54 +0200

    Skill List

--- ConsoleMon/ConsoleMon/ConsoleMon.cs
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -11,6 +11,10 @@ namespace ConsoleMon
       int Health = 5;
       int Energy = 0;
       string name = "licky";
+
+     List<Skill> Skills = new List<Skill>();
+
+       
      
 
         public void TakeDamage(int Damage)
@@ -23,5 +27,7 @@ namespace ConsoleMon
         {
             Energy =- energy;
         }
+
+       
     }
 }
